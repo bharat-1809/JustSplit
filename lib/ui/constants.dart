f323ed96cc6af8a39a39a835982a2c50915cbf27
Bharat Sharma <bharat.sharma1809@gmail.com>
2021-06-14 23:11:47 +0530

    bump v1.1.0+112 (#52)
    * ptch: update links and change razorpay error methods
    
    * workflow: update to use latest flutter version
    
    * bump v1.1.0+112

--- lib/ui/constants.dart
+++ lib/ui/constants.dart
@@ -13,7 +13,7 @@ final kInputBorderStyle = OutlineInputBorder(
 final String kRazorpayKey =
     'rzp_live_nu3POGoe0oSQh3'; //'rzp_test_XACVmXwOI28T9l';
 
-final String kAppVersion = "v1.1.0+111";
+final String kAppVersion = "v1.1.0+112";
 
 // The id of the dropdown item with name 'Add Friend' in add expenses page
 // This is to ensure that if a user cancels the process of adding a friend then
