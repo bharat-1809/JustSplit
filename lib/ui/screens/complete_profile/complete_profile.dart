48e0f359cc617f5ee93cf17a0baeeb43e3ee3aaf
Bharat Sharma <bharat.sharma1809@gmail.com>
2021-09-08 23:00:37 +0530

    fix: profile complete button error

--- lib/ui/screens/complete_profile/complete_profile.dart
+++ lib/ui/screens/complete_profile/complete_profile.dart
@@ -130,16 +130,15 @@ class _ProfileFormState extends State<ProfileForm> {
     void _onSaveButtonPressed() {
       if (!_formKey.currentState.validate() || _photoUrlController.text == null) return;
 
-      print(_phoneNumberController.text);
-      // BlocProvider.of<ProfileregBloc>(context).add(
-      //   ProfileRegClicked(
-      //     firstName: _firstNameController.text,
-      //     lastName: _lastNameController.text,
-      //     photoUrl: _photoUrlController.text,
-      //     phoneNumber: _phoneNumberController.text,
-      //     defaultCurrency: _defaultCurrencyController.text,
-      //   ),
-      // );
+      BlocProvider.of<ProfileregBloc>(context).add(
+        ProfileRegClicked(
+          firstName: _firstNameController.text,
+          lastName: _lastNameController.text,
+          photoUrl: _photoUrlController.text,
+          phoneNumber: _phoneNumberController.text,
+          defaultCurrency: _defaultCurrencyController.text,
+        ),
+      );
 
       currencySymbol = getCurrencySymbol(
         currencyData: _currencyData,
